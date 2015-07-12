6657d21e919a816eb780b392c2c4d5eb0fcff768
find-happiness <w564263331@gmail.com>
2015-07-12 11:06:14 +0800

    绘制完cube

--- Tutorial03/Tutorial03.cpp
+++ Tutorial03/Tutorial03.cpp
@@ -15,6 +15,7 @@
 struct SimpleVexter
 {
 	D3DXVECTOR3 pos;
+	D3DXVECTOR4 color;
 };
 
 HWND hWnd = NULL;
@@ -24,16 +25,28 @@ ID3D10RenderTargetView* g_pRenderTargetView = NULL;
 ID3D10Effect* g_pEffect;
 ID3D10EffectTechnique* g_pTechnique = NULL;
 ID3D10InputLayout* g_pInputLayout;
+ID3D10Buffer* g_pIndexBuffer;
+ID3D10Buffer* g_pVertexBuffer;
+
+ID3D10EffectMatrixVariable* g_pEffectMatrixVariableWorld = NULL;
+ID3D10EffectMatrixVariable* g_pEffectMatrixVariableView = NULL;
+ID3D10EffectMatrixVariable* g_pEffectMatrixVariablePrjection = NULL;
+
+D3DXMATRIX worldMatrix;
+D3DXMATRIX viewMatrix;
+D3DXMATRIX prejectionMatrix;
+
+D3D10_DRIVER_TYPE driverType;
 
 HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow);
 HRESULT InitDevice();
 void CleanupDevice();
 void Render();
-LRESULT CALLBACK WinProc(HWND,UINT,WPARAM,LPARAM);
+LRESULT CALLBACK WinProc(HWND, UINT, WPARAM, LPARAM);
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPreInstance, LPWSTR lpCmdLine, int nCmdShow)
 {
-	
+
 	UNREFERENCED_PARAMETER(hPreInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
@@ -63,10 +76,10 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPreInstance, LPWSTR lpCmdLin
 	CleanupDevice();
 
 	return (int)msg.wParam;
-		
+
 }
 
-HRESULT InitWindow(HINSTANCE hInstance,int nCmdShow)
+HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow)
 {
 	WNDCLASSEX wcex;
 	wcex.hInstance = hInstance;
@@ -89,17 +102,17 @@ HRESULT InitWindow(HINSTANCE hInstance,int nCmdShow)
 	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
 
 	hWnd = CreateWindow(L"DirectX10Tutorials", L"Tutorial03:draw cube", WS_OVERLAPPEDWINDOW,
-				CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top,NULL,NULL,hInstance,NULL);
+		CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, hInstance, NULL);
 
 	if (!hWnd)
 		return E_FAIL;
-	
+
 	ShowWindow(hWnd, nCmdShow);
 
 	return S_OK;
 }
 
-LRESULT CALLBACK WinProc(HWND hWnd,UINT message,WPARAM wParam,LPARAM lParam)
+LRESULT CALLBACK WinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	PAINTSTRUCT ps;
 	switch (message)
@@ -122,7 +135,7 @@ LRESULT CALLBACK WinProc(HWND hWnd,UINT message,WPARAM wParam,LPARAM lParam)
 HRESULT InitDevice()
 {
 	//1.swap china
-	
+
 	HRESULT hr = S_OK;
 
 	RECT rc;
@@ -158,12 +171,12 @@ HRESULT InitDevice()
 		D3D10_DRIVER_TYPE_HARDWARE,
 		D3D10_DRIVER_TYPE_REFERENCE,
 	};
-	D3D10_DRIVER_TYPE type;
+	
 	UINT numDriverType = sizeof(driveTypes) / sizeof(driveTypes[0]);
-	for (int d = 0; d < numDriverType;d++)
+	for (int d = 0; d < numDriverType; d++)
 	{
-		type = driveTypes[d];
-		hr = D3D10CreateDeviceAndSwapChain(NULL, type, NULL, createDriverFlag, D3D10_SDK_VERSION, &sd, &g_pSwapChain, &g_pD3dDevice);
+		driverType = driveTypes[d];
+		hr = D3D10CreateDeviceAndSwapChain(NULL, driverType, NULL, createDriverFlag, D3D10_SDK_VERSION, &sd, &g_pSwapChain, &g_pD3dDevice);
 		if (SUCCEEDED(hr))
 			break;
 	}
@@ -172,21 +185,21 @@ HRESULT InitDevice()
 		return hr;
 
 	//2.创建渲染目标
-	
+
 	ID3D10Texture2D* pBackBuffer;
 
 	hr = g_pSwapChain->GetBuffer(0, __uuidof(ID3D10Texture2D), (LPVOID*)(&pBackBuffer));
 
 	if (FAILED(hr))
 		return hr;
-	
+
 	hr = g_pD3dDevice->CreateRenderTargetView(pBackBuffer, NULL, &g_pRenderTargetView);
 	pBackBuffer->Release();
 
 	if (FAILED(hr))
 		return hr;
 
-	g_pD3dDevice->OMSetRenderTargets(1,&g_pRenderTargetView,NULL);
+	g_pD3dDevice->OMSetRenderTargets(1, &g_pRenderTargetView, NULL);
 
 	D3D10_VIEWPORT vp;
 	vp.Width = width;
@@ -209,7 +222,7 @@ HRESULT InitDevice()
 	dwShaderFlags |= D3D10_SHADER_DEBUG;
 #endif
 
-	hr = D3DX10CreateEffectFromFile(L"Tutorial03.fx", NULL, NULL, "fx_4_0", dwShaderFlags, 0, g_pD3dDevice,NULL, NULL, &g_pEffect,NULL,NULL);
+	hr = D3DX10CreateEffectFromFile(L"Tutorial03.fx", NULL, NULL, "fx_4_0", dwShaderFlags, 0, g_pD3dDevice, NULL, NULL, &g_pEffect, NULL, NULL);
 	if (FAILED(hr))
 	{
 		MessageBox(NULL, L"effect file create failed!", L"error", MB_OK);
@@ -220,55 +233,106 @@ HRESULT InitDevice()
 	g_pTechnique = g_pEffect->GetTechniqueByName("Render");
 	if (!g_pTechnique)
 		return E_FAIL;
-	
-	D3D10_INPUT_ELEMENT_DESC layout[] = 
+
+	g_pEffectMatrixVariableWorld = g_pEffect->GetVariableByName("World")->AsMatrix();
+	g_pEffectMatrixVariableView = g_pEffect->GetVariableByName("View")->AsMatrix();
+	g_pEffectMatrixVariablePrjection = g_pEffect->GetVariableByName("Projection")->AsMatrix();
+
+	D3D10_INPUT_ELEMENT_DESC layout[] =
 	{
-		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D10_INPUT_PER_VERTEX_DATA ,0},
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D10_INPUT_PER_VERTEX_DATA, 0 },
+		{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D10_INPUT_PER_VERTEX_DATA, 0 },
 	};
 
 	UINT numElements = sizeof(layout) / sizeof(layout[0]);
 
-	D3D10_PASS_DESC passDesc;
-	g_pTechnique->GetPassByIndex(0)->GetDesc(&passDesc);
-
-	hr = g_pD3dDevice->CreateInputLayout(layout, numElements, passDesc.pIAInputSignature, passDesc.IAInputSignatureSize, &g_pInputLayout);
-	
+	// Create the input layout
+	D3D10_PASS_DESC PassDesc;
+	g_pTechnique->GetPassByIndex(0)->GetDesc(&PassDesc);
+	hr = g_pD3dDevice->CreateInputLayout(layout, numElements, PassDesc.pIAInputSignature,
+		PassDesc.IAInputSignatureSize, &g_pInputLayout);
 	if (FAILED(hr))
 		return hr;
 
+	// Set the input layout
 	g_pD3dDevice->IASetInputLayout(g_pInputLayout);
 
-	SimpleVexter vexters[] = 
+	SimpleVexter vertices[] =
 	{
-		D3DXVECTOR3(0.0f, 0.5f, 0.5f),
-		D3DXVECTOR3(0.5f, -0.5f, 0.5f),
-		D3DXVECTOR3(-0.5f, -0.5f, 0.5f),
+		{ D3DXVECTOR3(-1.0f, 1.0f, -1.0f), D3DXVECTOR4(0.0f, 0.0f, 1.0f, 1.0f) },
+		{ D3DXVECTOR3(1.0f, 1.0f, -1.0f), D3DXVECTOR4(0.0f, 1.0f, 0.0f, 1.0f) },
+		{ D3DXVECTOR3(1.0f, 1.0f, 1.0f), D3DXVECTOR4(0.0f, 1.0f, 1.0f, 1.0f) },
+		{ D3DXVECTOR3(-1.0f, 1.0f, 1.0f), D3DXVECTOR4(1.0f, 0.0f, 0.0f, 1.0f) },
+		{ D3DXVECTOR3(-1.0f, -1.0f, -1.0f), D3DXVECTOR4(1.0f, 0.0f, 1.0f, 1.0f) },
+		{ D3DXVECTOR3(1.0f, -1.0f, -1.0f), D3DXVECTOR4(1.0f, 1.0f, 0.0f, 1.0f) },
+		{ D3DXVECTOR3(1.0f, -1.0f, 1.0f), D3DXVECTOR4(1.0f, 1.0f, 1.0f, 1.0f) },
+		{ D3DXVECTOR3(-1.0f, -1.0f, 1.0f), D3DXVECTOR4(0.0f, 0.0f, 0.0f, 1.0f) },
 	};
 
-	ID3D10Buffer* vexterBuffer;
 	D3D10_BUFFER_DESC bd;
 	bd.Usage = D3D10_USAGE_DEFAULT;
+	bd.ByteWidth = sizeof(SimpleVexter) * 8;
 	bd.BindFlags = D3D10_BIND_VERTEX_BUFFER;
-	bd.ByteWidth = sizeof(SimpleVexter) * 3;
 	bd.CPUAccessFlags = 0;
 	bd.MiscFlags = 0;
-
-	D3D10_SUBRESOURCE_DATA initData;
-	initData.pSysMem = vexters;
-
-	hr = g_pD3dDevice->CreateBuffer(&bd, &initData, &vexterBuffer);
-
+	D3D10_SUBRESOURCE_DATA InitData;
+	InitData.pSysMem = vertices;
+	hr = g_pD3dDevice->CreateBuffer(&bd, &InitData, &g_pVertexBuffer);
 	if (FAILED(hr))
-	{
 		return hr;
-	}
 
-	UINT strids = sizeof(SimpleVexter);
+	// Set vertex buffer
+	UINT stride = sizeof(SimpleVexter);
 	UINT offset = 0;
+	g_pD3dDevice->IASetVertexBuffers(0, 1, &g_pVertexBuffer, &stride, &offset);
 
-	g_pD3dDevice->IASetVertexBuffers(0, 1, &vexterBuffer, &strids, &offset);
-	
+	DWORD indices[] =
+	{
+		3, 1, 0,
+		2, 1, 3,
+
+		0, 5, 4,
+		1, 5, 0,
+
+		3, 4, 7,
+		0, 4, 3,
+
+		1, 6, 5,
+		2, 6, 1,
+
+		2, 7, 6,
+		3, 7, 2,
+
+		6, 4, 5,
+		7, 4, 6,
+
+	};
+
+	bd.Usage = D3D10_USAGE_DEFAULT;
+	bd.ByteWidth = sizeof(DWORD) * 36;        // 36 vertices needed for 12 triangles in a triangle list
+	bd.BindFlags = D3D10_BIND_INDEX_BUFFER;
+	bd.CPUAccessFlags = 0;
+	bd.MiscFlags = 0;
+	InitData.pSysMem = indices;
+	hr = g_pD3dDevice->CreateBuffer(&bd, &InitData, &g_pIndexBuffer);
+	if (FAILED(hr))
+		return hr;
+
+	// Set index buffer
+	g_pD3dDevice->IASetIndexBuffer(g_pIndexBuffer, DXGI_FORMAT_R32_UINT, 0);
+
+	// Set primitive topology
 	g_pD3dDevice->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	D3DXMatrixIdentity(&worldMatrix);
+
+	D3DXVECTOR3 eye(0.0f, 1.0f, -5.0f);
+	D3DXVECTOR3 up(0.0f, 1.0f, 0.0f);
+	D3DXVECTOR3 at(0.0f, 1.0f, 0.0f);
+
+	D3DXMatrixLookAtLH(&viewMatrix, &eye, &at, &up);
+
+	D3DXMatrixPerspectiveFovLH(&prejectionMatrix, (float)D3DX_PI *0.5f, width / (FLOAT)height, 0.1f, 100.0f);
 
 	return S_OK;
 }
@@ -287,23 +351,47 @@ void CleanupDevice()
 		g_pSwapChain->Release();
 	if (g_pD3dDevice)
 		g_pD3dDevice->Release();
+	if (g_pIndexBuffer)
+		g_pIndexBuffer->Release();
 
 }
 
 void Render()
 {
-	
+	static float t = 0.0f;
+	if (driverType == D3D10_DRIVER_TYPE_REFERENCE)
+	{
+		t += (float)D3DX_PI * 0.0125f;
+	}
+	else
+	{
+		static DWORD dwTimeStart = 0;
+		DWORD dwTimerCur = GetTickCount();
+		if (dwTimeStart == 0)
+		{
+			dwTimeStart = dwTimerCur;
+		}
+		t = (dwTimerCur - dwTimeStart) / 1000.f;
+	}
+
+	D3DXMatrixRotationY(&worldMatrix, t);
+
 	float clearColor[] = { 1.0f, 0.0f, 0.0f, 1.0f };
 
 	g_pD3dDevice->ClearRenderTargetView(g_pRenderTargetView, clearColor);
 
+
+	g_pEffectMatrixVariablePrjection->SetMatrix((float*)&prejectionMatrix);
+	g_pEffectMatrixVariableView->SetMatrix((float*)&viewMatrix);
+	g_pEffectMatrixVariableWorld->SetMatrix((float*)worldMatrix);
+
 	D3D10_TECHNIQUE_DESC td;
 	g_pTechnique->GetDesc(&td);
 
-	for (UINT i = 0; i < td.Passes;i++)
+	for (UINT i = 0; i < td.Passes; i++)
 	{
 		g_pTechnique->GetPassByIndex(i)->Apply(0);
-		g_pD3dDevice->Draw(3, 0);
+		g_pD3dDevice->DrawIndexed(36, 0, 0);
 	}
 
 	g_pSwapChain->Present(0, 0);
--- Tutorial03/Tutorial03.fx
+++ Tutorial03/Tutorial03.fx
@@ -1,13 +1,48 @@
-float4 VS(float4 Pos : POSITION) : SV_POSITION
+//--------------------------------------------------------------------------------------
+// File: Tutorial04.fx
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//--------------------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------------------
+// Constant Buffer Variables
+//--------------------------------------------------------------------------------------
+matrix World;
+matrix View;
+matrix Projection;
+
+
+//--------------------------------------------------------------------------------------
+struct VS_OUTPUT
 {
-	return Pos;
+	float4 Pos : SV_POSITION;
+	float4 Color : COLOR0;
+};
+
+//--------------------------------------------------------------------------------------
+// Vertex Shader
+//--------------------------------------------------------------------------------------
+VS_OUTPUT VS(float4 Pos : POSITION, float4 Color : COLOR)
+{
+	VS_OUTPUT output = (VS_OUTPUT)0;
+	output.Pos = mul(Pos, World);
+	output.Pos = mul(output.Pos, View);
+	output.Pos = mul(output.Pos, Projection);
+	output.Color = Color;
+	return output;
 }
 
-float4 PS(float4 Pos :SV_POSITION) :SV_Target
+
+//--------------------------------------------------------------------------------------
+// Pixel Shader
+//--------------------------------------------------------------------------------------
+float4 PS(VS_OUTPUT input) : SV_Target
 {
-	return float4(0.0f, 1.0f, 0.0f, 1.0f);
+	return input.Color;
 }
 
+
+//--------------------------------------------------------------------------------------
 technique10 Render
 {
 	pass P0
@@ -17,3 +52,4 @@ technique10 Render
 		SetPixelShader(CompileShader(ps_4_0, PS()));
 	}
 }
+
